6db6da7dffb3ced8c47fdcc06ec92e00ffd730ba
hkc848 <hkc848@mun.ca>
2014-11-20 16:01:16 -0330

    Update Skill.java

--- src/Skill.java
+++ src/Skill.java
@@ -1,23 +1,34 @@
-public abstract class Skill
+public class Skill
 {
-	private int mark;
 	private String name;
-	public Skill(String name,int mark)
+	private int rate;
+	private int convert(final Rate rate)
 	{
-		this.mark=mark;
-		this.name=name;
+		switch(rate)
+		{
+		case BAD:			return 1;
+		case NORMAL:		return 2;
+		case GOOD:			return 3;
+		default:			return 0;
+		}
 	}
-	public double getMark()
+	public static enum Rate{BAD,NORMAL,GOOD};
+	public Skill(String name,final Rate rate)
 	{
-		return mark;
+		this.name=name;
+		this.rate=convert(rate);
+	}
+	public int getRate()
+	{
+		return rate;
 	}
 	public String getSkill()
 	{
 		return name;
 	}
-	public void setMark(int mark)
+	public void setMark(final Rate rate)
 	{
-		this.mark=mark;
+		this.rate=convert(rate);
 	}
 	public void setSkill(String name)
 	{
