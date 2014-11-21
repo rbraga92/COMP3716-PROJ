c00aaccbf91e6e4a729d77824f9b42992e8d3409
hkc848 <hkc848@mun.ca>
2014-11-21 10:00:48 -0330

    Update by hkc.

--- src/Generator.java
+++ src/Generator.java
@@ -23,6 +23,9 @@ public class Generator{
     	}
     	//ifAimLow it requires the floor of students.size()/groupSize + 1 number of groups/
     	//Example : for groups of 4, 9 students:  size = 9, 9/4 =2; 2+1=3, 3 groups.
+    	
+    	//			I have question. How about 8 students? 8 students will cause 3 groups, too.
+    	
     	//else, it requires the floor of students/(groupSize) groups.
     	//example: for groups of 4, 9 students, size = 9;  9/4 = 2; 2 groups.
     	int noOfGroups=0;
@@ -32,12 +35,22 @@ public class Generator{
         	groups = new ArrayList<Group>(students.size()/groupSize);
     	}
     	groups = new ArrayList<Group>(noOfGroups);
+    	
+    	//You assign groups to a new ArrayList.
+    	//So, what is the function of line 33 and 35?
+    	
     	for (int i=0; i<students.size();i++){ 
     		//adds student i to the group that is numbered by
     		// i%groups.size().  This will generate 
 			groups.get(i%(groups.size())).addStudent(students.get(i));
 		}
 		return groups;
+		
+		//There is a group field called "groups".
+		//But you didn't use any get method to access this field.
+		//So, why do you assign to "groups" in generate method?
+		//You could just return the groups you create, right?
+		//Is that correct?
     }
     
     public ArrayList<Student> getStudents(){
